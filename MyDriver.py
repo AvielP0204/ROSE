d7f989f20021c594cffab81a7e71bbf226d112a1
AvielP0204 <avielpiven0204@gmail.com>
2023-08-06 16:31:19 +0300

    added do_action and changed the car's name

--- MyDriver.py
+++ MyDriver.py
@@ -3,8 +3,26 @@ This driver does not do any action.
 """
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "No Driver"
+driver_name = "EpicAwesome" #do recommend better names in the whatasapp group chat :D
 
 
 def drive(world):
-    return actions.NONE
+    try:
+        obstacles = world.get ((x, y - 1))
+    except IndexError:
+        pass
+    else:
+        do_action(obstacle)
+
+## this function decides what action to do based on the obstacle thats directly infront of it
+def do_action(obstacle):
+    if obstacle == obstacles.WATER:
+        return actions.BRAKE
+    elif obstacle == obstacles.PENGUIN:
+        return actions.PICKUP
+    elif obstacle == obstacles.CRACK:
+        return actions.JUMP
+    elif obstacle == obstacles.BIKE or obstacle == obstacles.TRASH or obstacle == obstacles.BARRIER:
+        pass
+        #TODO: write logic for which direction to choose
+
